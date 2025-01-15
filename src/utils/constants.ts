d69a5336edb5212762b0bc5ea35eb3bbcba67bea
alvseven <alv7contato@outlook.com>
2025-01-15 08:33:32 -0300

    refactor: fix typo

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 // The JS version of this file is generated manually by scripts/postbuild.js
 
 /** The name of the package */
-export const PKG_NAME = 'mediucs';
+export const PKG_NAME = 'medicus';
 
 /** The version of the package */
 export const PKG_VERSION = '0.0.0'; // changed by scripts/postbuild.js
