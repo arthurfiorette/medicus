b4f0c4911257c9129077fcdab6a92daa7e90bd56
Arthur Fiorette <me@arthur.place>
2025-01-23 12:59:29 -0300

    feat: typesafe patch

--- src/integrations/otel.ts
+++ src/integrations/otel.ts
@@ -1,7 +1,9 @@
 import { SpanKind, SpanStatusCode, context, trace } from '@opentelemetry/api';
+import { Medicus } from '../medicus';
 import { definePlugin } from '../plugins';
 import { HealthStatus } from '../types';
 import { PKG_NAME, PKG_VERSION } from '../utils/constants';
+import { patch } from '../utils/patch';
 
 export enum MedicusAttributesNames {
   /**
@@ -18,6 +20,12 @@ export enum MedicusAttributesNames {
   CHECKER_STATUS = 'medicus.checker_status'
 }
 
+// just to make the methods public
+declare class PublicMedicus<Ctx> extends Medicus<Ctx> {
+  public executeChecker: Medicus<Ctx>['executeChecker'];
+  public performBackgroundCheck: Medicus<Ctx>['performBackgroundCheck'];
+}
+
 /**
  * A plugin that integrates OpenTelemetry tracing into the Medicus system
  *
@@ -28,93 +36,122 @@ export const openTelemetryMedicusPlugin = definePlugin<void>(() => ({
   created(medicus) {
     const tracer = trace.getTracer(PKG_NAME, PKG_VERSION);
 
-    if (medicus.onBackgroundCheck) {
-      const old = medicus.onBackgroundCheck;
-      medicus.onBackgroundCheck = function otelOnBackgroundCheck(result) {
-        const span = tracer.startSpan('medicus.onBackgroundCheck', undefined, context.active());
-        try {
-          return context.with(trace.setSpan(context.active(), span), old, undefined, result);
-        } finally {
-          span.end();
+    patch(
+      medicus,
+      'onBackgroundCheck',
+      (onBackgroundCheck) =>
+        function otelOnBackgroundCheck(result) {
+          const span = tracer.startSpan('medicus.onBackgroundCheck', undefined, context.active());
+          try {
+            return context.with(
+              trace.setSpan(context.active(), span),
+              onBackgroundCheck,
+              undefined,
+              result
+            );
+          } finally {
+            span.end();
+          }
         }
-      };
-    }
+    );
 
-    const oldPerformCheck = medicus.performCheck;
-    medicus.performCheck = function otelPerformCheck(debug?: boolean) {
-      const span = tracer.startSpan(
-        'medicus.performCheck',
-        { attributes: { [MedicusAttributesNames.DEBUG]: !!debug } },
-        context.active()
-      );
+    patch(
+      medicus,
+      'performCheck',
+      (performCheck) =>
+        function otelPerformCheck(debug?: boolean) {
+          const span = tracer.startSpan(
+            'medicus.performCheck',
+            { attributes: { [MedicusAttributesNames.DEBUG]: !!debug } },
+            context.active()
+          );
 
-      try {
-        return context.with(trace.setSpan(context.active(), span), oldPerformCheck, this, debug);
-      } catch (error: any) {
-        span.setStatus({
-          code: SpanStatusCode.ERROR,
-          message: error.message
-        });
+          try {
+            return context.with(trace.setSpan(context.active(), span), performCheck, this, debug);
+          } catch (error: any) {
+            span.setStatus({
+              code: SpanStatusCode.ERROR,
+              message: error.message
+            });
 
-        span.recordException(error);
+            span.recordException(error);
 
-        throw error;
-      } finally {
-        span.end();
-      }
-    };
-
-    //@ts-expect-error - protected property
-    const oldExecuteChecker = medicus.executeChecker;
-    //@ts-expect-error - protected property
-    medicus.executeChecker = async function otelExecuteChecker(args) {
-      const span = tracer.startSpan(
-        `medicus.checker:${args[0]}`,
-        { attributes: { [MedicusAttributesNames.CHECKER_NAME]: args[0] }, kind: SpanKind.PRODUCER },
-        context.active()
-      );
-
-      try {
-        // this function never throws
-        const result = await context.with(
-          trace.setSpan(context.active(), span),
-          oldExecuteChecker,
-          this,
-          args
-        );
-
-        span.setAttributes({
-          [MedicusAttributesNames.CHECKER_STATUS]: result[1].status,
-          ...result[1].debug
-        });
-
-        // Unhealthy should mark as error
-        if (result[1].status !== HealthStatus.HEALTHY) {
-          span.setStatus({ code: SpanStatusCode.ERROR });
+            throw error;
+          } finally {
+            span.end();
+          }
         }
+    );
 
-        // Attaches a possible exception to the span
-        if (typeof result[1].debug?.error === 'string' || result[1].debug?.error instanceof Error) {
-          span.recordException(result[1].debug.error);
+    patch(
+      medicus as PublicMedicus<typeof medicus.context>,
+      'executeChecker',
+      (executeChecker) =>
+        async function otelExecuteChecker(args) {
+          const span = tracer.startSpan(
+            `medicus.checker:${args[0]}`,
+            {
+              attributes: { [MedicusAttributesNames.CHECKER_NAME]: args[0] },
+              kind: SpanKind.PRODUCER
+            },
+            context.active()
+          );
+
+          try {
+            // this function never throws
+            const result = await context.with(
+              trace.setSpan(context.active(), span),
+              executeChecker,
+              this,
+              args
+            );
+
+            span.setAttributes({
+              [MedicusAttributesNames.CHECKER_STATUS]: result[1].status,
+              ...result[1].debug
+            });
+
+            // Unhealthy should mark as error
+            if (result[1].status !== HealthStatus.HEALTHY) {
+              span.setStatus({ code: SpanStatusCode.ERROR });
+            }
+
+            // Attaches a possible exception to the span
+            if (
+              typeof result[1].debug?.error === 'string' ||
+              result[1].debug?.error instanceof Error
+            ) {
+              span.recordException(result[1].debug.error);
+            }
+
+            return result;
+          } finally {
+            span.end();
+          }
         }
+    );
 
-        return result;
-      } finally {
-        span.end();
-      }
-    };
+    patch(
+      medicus as PublicMedicus<typeof medicus.context>,
+      'performBackgroundCheck',
+      (performBackgroundCheck) =>
+        async function otelPerformBackgroundCheck() {
+          const span = tracer.startSpan(
+            'medicus.performBackgroundCheck',
+            undefined,
+            context.active()
+          );
 
-    //@ts-expect-error - protected property
-    const oldPerformBackgroundCheck = medicus.performBackgroundCheck;
-    //@ts-expect-error - protected property
-    medicus.performBackgroundCheck = function otelPerformBackgroundCheck() {
-      const span = tracer.startSpan('medicus.performBackgroundCheck', undefined, context.active());
-
-      try {
-        return context.with(trace.setSpan(context.active(), span), oldPerformBackgroundCheck, this);
-      } finally {
-        span.end();
-      }
-    };
+          try {
+            return context.with(
+              trace.setSpan(context.active(), span),
+              performBackgroundCheck,
+              this
+            );
+          } finally {
+            span.end();
+          }
+        }
+    );
   }
 }));
